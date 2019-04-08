2147ef33b4f309c6504b364a6594056c4efb942c
Andreas Erhard <andreas.erhard@i-med.ac.at>
2019-04-08 12:53:59 +0200

    Add configuration profile for Symfony 4

--- scripts/setup-nginx.sh
+++ scripts/setup-nginx.sh
@@ -106,6 +106,41 @@ server {
 EOF
         sed -i -r 's/^(autostart=)false$/\1true/' /etc/supervisor/conf.d/php-fpm.conf
         ;;
+    'symfony4')
+        # This configuration profile sets up the NGINX webserver with PHP-FPM configured for symfony apps.
+        # Use case example: Full-stack symfony 4.x application.
+        cat <<EOF >/etc/nginx/conf.d/default.conf
+server {
+    listen 80;
+
+    root $WEBROOT;
+    index index.php;
+
+    include /etc/nginx/conf.d.server/blockdot.conf;
+    include /etc/nginx/conf.d.server/nolog_favrob.conf;
+    #include /etc/nginx/conf.d.server/docker_realip.conf;
+
+    ##PLACEHOLDER_CUSTOM_CONFIGURATION##
+
+    access_log /dev/stdout;
+    error_log /dev/stderr;
+
+    location / {
+        try_files \$uri /index.php\$is_args\$args;
+    }
+
+    location ~ ^/index\.php(/|$) {
+        include /etc/nginx/conf.d.server/phpfpm.conf;
+        internal;
+    }
+
+    location ~ \.php$ {
+        return 404;
+    }
+}
+EOF
+        sed -i -r 's/^(autostart=)false$/\1true/' /etc/supervisor/conf.d/php-fpm.conf
+        ;;
     *)
         (>&2 echo 'Invalid configuration profile!');
 
